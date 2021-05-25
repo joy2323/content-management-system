1f4fe68270e5ffd287c6f3362b17174f6ce5126c
joy2323 <joyjoel2011@gmail.com>
2021-05-25 09:40:07 +0100

    added screenshots

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('cmsimg') or 'ENTER_STORAGE_ACCOUNT_NAME'
+    BLOB_STORAGE_KEY = os.environ.get('5B3nk/TzqEDfOCVviRN7Fl4VqgaEc2QUTdRuEf7308yg+6yY5Tlpqb0y8hBfQOSm5NJhMfaLST+sdmwC+s7n8Q==') or 'ENTER_BLOB_STORAGE_KEY'
+    BLOB_CONTAINER = os.environ.get('images') or 'ENTER_IMAGES_CONTAINER_NAME'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('contentmanage') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
+    SQL_DATABASE = os.environ.get('cms') or 'ENTER_SQL_DB_NAME'
+    SQL_USER_NAME = os.environ.get('systemadmin') or 'ENTER_SQL_SERVER_USERNAME'
+    SQL_PASSWORD = os.environ.get('Content2020') or 'ENTER_SQL_SERVER_PASSWORD'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
